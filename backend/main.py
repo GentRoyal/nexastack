b328b57eae9b65256110c6aa99c44d40fa587e97
Ridwan Yusuf <ridwan.yusuf@whiteshield.com>
2026-06-28 23:05:19 +0100

    Updated vercel url

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://nexastack-g3ntroyal.vercel.app/",  # replace with your actual Vercel URL
+        "https://nexastack-weld.vercel.app/", 
     ],
     allow_methods=["*"],
     allow_headers=["*"],
